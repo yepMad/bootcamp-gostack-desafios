1ac63db13277649bceb6983ac47d21e4151fdcce
Eudes Tenório <eudestfilho@gmail.com>
2020-07-13 04:28:50 -0300

    Finalizado e de acordo com o Jest

--- gostack-desafio-2-conceitos-nodejs/src/app.js
+++ gostack-desafio-2-conceitos-nodejs/src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,85 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const newRepositorie = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+
+  repositories.push(newRepositorie);
+
+  return response.status(200).json(newRepositorie);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { body, params } = request;
+
+  const { title, url, techs } = body;
+  const { id } = params;
+
+  const oldRepositorieIndex = repositories.findIndex(element => element.id === id);
+
+  if(oldRepositorieIndex === -1) {
+    return response.status(404).json({
+      success: false,
+      message: "Repositório não encontrado"
+    });
+  }
+
+  const oldRepositorie = repositories[oldRepositorieIndex];
+
+  repositories[oldRepositorieIndex] = {
+    id: oldRepositorie.id,
+    title: title || oldRepositorie.title,
+    url: url || oldRepositorie.url,
+    techs: techs || oldRepositorie.techs,
+    likes: oldRepositorie.likes
+  }
+
+  return response.status(200).json(repositories[oldRepositorieIndex]);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositorieIndex = repositories.findIndex(element => element.id === id);
+
+  if(repositorieIndex === -1) {
+    return response.status(404).json({
+      success: false,
+      message: "Repositório não encontrado"
+    });
+  }
+
+  repositories.splice(repositorieIndex, 1);
+
+  return response.status(204).json();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositorieIndex = repositories.findIndex(element => element.id === id);
+
+  if(repositorieIndex === -1) {
+    return response.status(404).json({
+      success: false,
+      message: "Repositório não encontrado"
+    });
+  }
+
+  repositories[repositorieIndex].likes += 1;
+
+  return response.status(200).json(repositories[repositorieIndex]);
 });
 
 module.exports = app;
